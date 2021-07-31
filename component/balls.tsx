e715fa57228a3360b1efb96a8e99cfafcf16b2c3
George Fischer <me@george.id.au>
2021-07-31 14:02:03 +1000

    max width

--- component/balls.tsx
+++ component/balls.tsx
@@ -1,6 +1,6 @@
 const Ball = () => <div style={{ background: 'red', width: '20px', height: '20px', borderRadius: '9999px' }} />;
 
-const BallCounter: React.FunctionComponent<{ balls: number }> = ({ balls }) => <div style={{ display: 'flex' }}>
+const BallCounter: React.FunctionComponent<{ balls: number }> = ({ balls }) => <div style={{ display: 'flex', flexWrap: 'wrap', maxWidth: '500px' }}>
     {[...new Uint8Array(balls ?? 0)].map((i, n) => <Ball key={n} />)}
 </div>;
 
