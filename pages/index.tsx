eb9ef27a0d5007e756cf4e6af57c3a177af0b56b
George Fischer <me@george.id.au>
2021-08-01 13:38:52 +1000

    saving

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BallMover from "../component/ballmover";
 import BallCounter from "../component/balls";
 import BallsContext from "../component/ballsContext";
 import Head from "next/head";
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 const HomePage = () => {
   const [balls, setBalls] = React.useState<number>(0);
+  useEffect(() => {
+    if (localStorage.getItem("balls")) {
+      const b = JSON.parse(localStorage.getItem("balls") ?? balls.toString());
+      setBalls(b ?? 0);
+    }
+  }, [balls]);
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +23,11 @@ const HomePage = () => {
       <div className={styles.header}>{balls} Balls</div>
       <BallCounter balls={balls} />
       <BallsContext.Provider
-        value={{ balls, update: (v) => setBalls(Math.max(v, 0)) }}
+        value={{ balls, update: (v) => {
+          let nb = Math.max(v, 0);
+          setBalls(nb);
+          localStorage.setItem('balls', JSON.stringify(nb))
+        } }}
       >
         <BallMover />
       </BallsContext.Provider>
