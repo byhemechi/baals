e5a6793c1b1a3297b9f6a167b2b8595f71dd57a0
George Fischer <me@george.id.au>
2021-08-01 16:57:24 +1000

    mobile support

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,9 +20,37 @@ const HomePage = () => {
           {balls == 0 ? "No" : balls} ball{balls == 1 ? "" : "s"}
         </title>
         <meta name="description" content="balls" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#111827" />
+        <meta name="msapplication-TileColor" content="#111827" />
+        <meta name="theme-color" content="#111827" />
       </Head>
       <div className={styles.header}>
-        <button className={styles.resetButton} onClick={() => {localStorage.removeItem('balls'); setBalls(0)}}>
+        <button
+          className={styles.resetButton}
+          onClick={() => {
+            localStorage.removeItem("balls");
+            setBalls(0);
+          }}
+        >
           {balls == 0 ? "No" : balls} ball{balls == 1 ? "" : "s"}
         </button>
       </div>
